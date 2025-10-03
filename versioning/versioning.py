b036f68ab9b9ded6f173bef97cb32b8b94da8396
Michal Boguslawski <mboguslawski@griddynamics.com>
2025-10-03 12:54:32 +0200

    Save

---
+++ versioning/versioning.py
@@ -0,0 +1,48 @@
+from semver import VersionInfo
+
+import subprocess
+import sys
+import os
+
+VERSION_FILE = "VERSION"
+
+def get_current_version():
+    if not os.path.exists(VERSION_FILE):
+        return VersionInfo.parse("0.0.0")
+    else:
+        with open(VERSION_FILE) as f:
+            return VersionInfo.parse(f.read().strip())
+
+
+def bump_version_tag(seman_version_type):
+    current_ver = get_current_version()
+    if seman_version_type == "major":
+        new_version = current_ver.bump_major()
+    elif seman_version_type == "minor":
+        new_version = current_ver.bump_minor()
+    elif seman_version_type == "patch":
+        new_version = current_ver.bump_patch()
+    else:
+        print("Please provide correct semantic version type: major, minor or patch")
+        exit()
+    with open(VERSION_FILE, "w") as f:
+        f.write(str(new_version))
+
+    
+
+    # Adding git tag ..
+    subprocess.run(["git", "tag", str(new_version)])
+    # Bumped version to new_version
+    subprocess.run(["git", "push", "--tags"])
+
+    # Adding VERSION file modifications to the last pushed commit
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "--amend", "--no-edit", "--quiet"])
+    subprocess.run(["git", "push", "--force", "--quiet"])
+
+    print(str(new_version))
+    return str(new_version)
+
+if __name__ == "__main__":
+    seman_version_type = sys.argv[1]
+    bump_version_tag(seman_version_type)
